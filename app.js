a8449302ccdf0d0bd9ac745112a4ae63776bd8c4
Ardian Haxha <ardian@brainpress.org>
2016-07-01 04:07:06 +0200

    Creating gulp tasks, setting a jshint and jscs standard, injecting jquery and bootstrap javascript from gulp task

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 
-
 var app = express();
 
 var port = 5000;
@@ -9,18 +8,15 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
-	res.send('Hello World');
+app.get('/', function (req, res) {
+    res.send('Hello World');
 });
 
-app.get('/books', function(req, res){
-	res.send('Hello Books');
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
 });
 
-
-
-
-app.listen(5000, function(err){
+app.listen(5000, function (err) {
     console.log('running server on port ' + port);
 
 });
