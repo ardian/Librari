56fbd9fb637ae5269950403de0488441be7d97ea
Ardian Haxha <ardian@brainpress.org>
2016-07-03 02:43:13 +0200

    Adding and configuring jade template engine

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var port = process.env.PORT || 5000;
 
 // app.use is prior to app.get
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', ('./src/views'));
+
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.get('/books', function (req, res) {
