1514780570adfa4d6d70bcd677421ce4446d398c
Ardian Haxha <ardian@brainpress.org>
2016-07-03 03:34:36 +0200

    Adding handlebars, changing from jade to hbs, adding test hbs file

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', ('./src/views'));
 
-app.set('view engine', 'jade');
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    res.render('index', {numbers: [1,2,3,4, 5]});
+    res.render('index', {title: 'Hello from render', numbers: [1,2,3,4, 5]});
 });
 
 app.get('/books', function (req, res) {
