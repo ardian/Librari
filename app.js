eb94f4299fa792bccaefa6c14694703ef3cd6350
Ardian Haxha <ardian@brainpress.org>
2016-07-04 03:17:33 +0200

    Got out router working, and setup our first route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
+var bookRouter = express.Router();
+
 // app.use is prior to app.get
 app.use(express.static('public'));
 app.set('views', ('./src/views'));
@@ -13,6 +15,19 @@ app.set('views', ('./src/views'));
 // app.engine('.hbs', handlebars({extname: '.hbs'}));
 
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Books');
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Hello Single Book');
+    });
+
+// Everything that uses bookRouter will work under /Books
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hello from render', nav: [{ Link: '/Books', Text: 'Books'}, { Link: '/Authors', Text: 'Authors' }]});
