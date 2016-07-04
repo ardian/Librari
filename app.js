56ae49c77300be57798fd86c43daffb80112e76b
Ardian Haxha <ardian@brainpress.org>
2016-07-04 03:05:23 +0200

    Simple navigation

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', ('./src/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello from render', numbers: [1,2,3,4, 5]});
+    res.render('index', { title: 'Hello from render', nav: [{ Link: '/Books', Text: 'Books'}, { Link: '/Authors', Text: 'Authors' }]});
 });
 
 app.get('/books', function (req, res) {
